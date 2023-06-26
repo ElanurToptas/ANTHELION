b0ce8b04de57353338c6972b10b7a4d861157c1c
retropol <mehmet.demirturk@ogr.sakarya.edu.tr>
2023-06-26 22:40:22 +0300

    sayfalar_bağlandı

--- lib/tasarım_UI/bottom_bar.dart
+++ lib/tasarım_UI/bottom_bar.dart
@@ -1,41 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:petcare/tasarım_UI/home_page.dart';
+import 'package:petcare/tasarım_UI/veterinarians_page.dart';
+import 'package:petcare/tasarım_UI/services_page.dart';
+import 'package:petcare/vet_pages/vet._main.dart';
+
+void main() {
+  runApp(BottomBarUI());
+}
 
 class BottomBarUI extends StatefulWidget {
-  const BottomBarUI({super.key});
+  const BottomBarUI({Key? key});
 
   @override
   State<BottomBarUI> createState() => _BottomBarUIState();
 }
 
 class _BottomBarUIState extends State<BottomBarUI> {
+  int _selectedIndex = 0; // Seçilen sekme indeksi
+  final List<Widget> _pageOptions = [
+    // İçerik sayfaları
+    HomePage(),
+    VeterinariansPage(),
+    ServicesPage(),
+    ProfilePage(),
+  ];
+
+  void _onTabChanged(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: Container( color: Colors.transparent,
-          child: Padding(
-            padding: const EdgeInsets.all(10),
-            child: GNav(
-             //backgroundColor: Colors.indigo,
-             color: Colors.indigo,
-             activeColor: Colors.white,
-             tabBackgroundColor: Colors.indigoAccent,
-             onTabChange: (index) {
-              print(index);
-             },
-              padding: EdgeInsets.all(20),
-              
-              tabs: const[
-                GButton(icon: Icons.home_rounded, text: "Ana Sayfa", ),
-                GButton(icon: Icons.local_hospital_rounded, text: "Veterinerler",),
-                GButton(icon: Icons.apartment_rounded, text: "Hizmetler",),
-                GButton(icon: Icons.pets_rounded ,text: "Profilim",),
-              
-              ],
-              ),
+      bottomNavigationBar: Container(
+        color: Colors.transparent,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: GNav(
+            color: Colors.indigo,
+            activeColor: Colors.white,
+            tabBackgroundColor: Colors.indigoAccent,
+            selectedIndex: _selectedIndex,
+            onTabChange: _onTabChanged,
+            padding: EdgeInsets.all(20),
+            tabs: const [
+              GButton(icon: Icons.home_rounded, text: "Ana Sayfa"),
+              GButton(icon: Icons.local_hospital_rounded, text: "Veterinerler"),
+              GButton(icon: Icons.apartment_rounded, text: "Hizmetler"),
+              GButton(icon: Icons.pets_rounded, text: "Profilim"),
+            ],
           ),
         ),
-      );
-   
+      ),
+      body: _pageOptions[
+          _selectedIndex], // Seçilen sekme için ilgili içeriği gösterir
+    );
   }
 }
--- lib/vet_pages/vet._main.dart
+++ lib/vet_pages/vet._main.dart
@@ -1,1 +1,37 @@
-﻿
+﻿import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Veterinarian Profile'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(),
+              ),
+              ElevatedButton(
+                child: Text('Hayvan Çip No'),
+                onPressed: () {
+                  // Hayvan çip no butonuna tıklanınca yapılacak işlemler
+                  // Buraya istediğiniz kodları ekleyebilirsiniz
+                },
+              ),
+              ElevatedButton(
+                child: Text('Profili Düzenle'),
+                onPressed: () {
+                  // Profili düzenle butonuna tıklanınca yapılacak işlemler
+                  // Buraya istediğiniz kodları ekleyebilirsiniz
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
