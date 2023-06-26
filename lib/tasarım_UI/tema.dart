21182efb06d0378913e1f60c584eed28c58d7a06
Fatma Eser <115644915+eserekli@users.noreply.github.com>
2023-06-27 00:55:38 +0300

    Tema_Oluşturuldu_ Yazı_ Fontu_Hatası_Çözüldü

--- lib/tasarım_UI/tema.dart
+++ lib/tasarım_UI/tema.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class TemaUI {
-  static final ThemeData lightTheme = ThemeData(
+  static  ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
     primaryColor: Colors.indigo,
-    fontFamily: GoogleFonts.getFont('Poppins'),
     );
+  
+  TextStyle HeaderOne = GoogleFonts.poppins(
+    color: Colors.black,
+    fontSize: 27,
+    fontWeight: FontWeight.w800,
+  );
   
 }
