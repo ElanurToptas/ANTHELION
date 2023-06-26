4b9a1cf5e7efb59b1d19c2f41de4b98b2e948d50
Fatma Eser <115644915+eserekli@users.noreply.github.com>
2023-06-27 02:33:00 +0300

    Assets_Eklendi

---
+++ lib/tasarim_UI/tema.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TemaUI {
+  static  ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primaryColor: Colors.indigo,
+    //fontFamily: baslik,
+    
+    );
+  
+ 
+  
+}
+
+ TextStyle baslik = GoogleFonts.poppins(
+    color: Colors.black,
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+  );
+
+  //AppBarTheme appbar = AppBarTheme();
