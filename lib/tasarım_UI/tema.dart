c374ac09f36eccb843d175c056281846ed22b5c1
Fatma Eser <115644915+eserekli@users.noreply.github.com>
2023-06-26 19:35:26 +0300

    Bottom_Bar_ Hata_Giderme

---
+++ lib/tasarım_UI/tema.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TemaUI {
+  static final ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primaryColor: Colors.indigo,
+    fontFamily: GoogleFonts.getFont('Poppins'),
+    );
+  
+}
