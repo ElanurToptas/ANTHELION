563593682cbeabf77a79effbee3de997e8e9c913
ArincSenturk <arincsenturk@gmail.com>
2023-06-27 19:42:06 +0300

    User profile folder created
    kullanıcı profil sayfaları klasörü yaratıldı kullanıcı profil sayfası yapıldı

--- lib/user_profile_pages/user_profile.dart
+++ lib/user_profile_pages/user_profile.dart
@@ -1,60 +1,107 @@
 import 'package:flutter/material.dart';
-
+// tema buton için
+class ButtonStyles {
+  static final elevatedButtonStyle = ElevatedButton.styleFrom(
+    backgroundColor:  Colors.deepPurple,
+    textStyle: TextStyle(fontSize: 18),
+    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 30),
+    minimumSize: Size(120, 120),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(8),
+    ),
+  );
+}
 class UserProfile extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Scaffold(
+    return Scaffold(backgroundColor: Color.fromARGB(255, 240, 240, 240),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(49, 123, 93, 255),
+        title: Text('Kullanıcı Profili'),
+      ),
       body: SafeArea(
         child: Column(
           children: [
-             Padding(
-               padding: const EdgeInsets.symmetric(vertical:20.0),
-               child: Image.network(
-                'https://hips.hearstapps.com/hmg-prod/images/man-with-dog-royalty-free-image-1582281437.jpg'
+            //********************************************* Resmin Olduğu Yer *********************************************************
+            Image.network(
+            'https://www.medivet.co.uk/globalassets/assets/shutterstock-and-istock/shutterstock_708732331.png?width=585',
+            width: 500, // Resmin genişliği
+            height: 350, // Resmin yüksekliği
+          ),
+          //**************************************************************************************************************************** 
+            Expanded(
+              child: Container(),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+              child: Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 10,
+                      offset: Offset(1, 1),
+                    ),
+                  ],
                 ),
-             ),
-             // 1. buton
-             SizedBox(height: 50, width: 200,
-               child: ElevatedButton(
-                style:ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple),
-                onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: ((context) {
-                  return UserProfile();
-                })));
-                         }, child: Text("Evcil Hayvan Karnesi")),
-             ),
-             SizedBox(height: 20,),//Butonlar arası boşluk
-            // 2. buton
-            SizedBox(height: 50, width: 200,
-              child: ElevatedButton(
-                style:ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple),
-                onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: ((context) {
-                  return UserProfile();
-                })));
-              }, child: Text("Geçmiş Randevularım")),
-            ),
-            SizedBox(height: 20,),//Butonlar arası boşluk
-            // 3. buton
-            SizedBox(height: 50, width: 200,
-              child: ElevatedButton(
-                style:ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple),
-                onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: ((context) {
-                  return UserProfile();
-                })));
-              }, child: Text("Tahlillerim")),
-            ),
-            SizedBox(height: 20,),//Butonlar arası boşluk
-            //4.buton
-            SizedBox(height: 50, width: 200,
-              child: ElevatedButton(
-                style:ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple),
-                onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: ((context) {
-                  return UserProfile();
-                })));
-              }, child: Text("Aşı takvimi")),
+                child: Column(
+                  children: [
+                    // ****************************************************** İLK SIRA BUTONLAR ***************************************
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(height: 120,width: 150,
+                          child: ElevatedButton(
+                            style: ButtonStyles.elevatedButtonStyle,
+                            onPressed: () {},
+                            child: Text('     Tahlil \n Sonuçları'),
+                          ),
+                        ),
+                        SizedBox(width: 16),
+                        SizedBox(height: 120,width: 150,
+                          child: ElevatedButton(
+                            style: ButtonStyles.elevatedButtonStyle,
+                            onPressed: () {
+                              
+                            },
+                            child: Text('Gelecek \n Aşıları'),
+                          ),
+                        ),
+                      ],
+                    ),
+                    //************************************************************************************************************************* 
+                    SizedBox(height: 10,),
+                    //************************************************* İKİNCİ SIRA BUTONLAR ************************************************
+                     Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ButtonStyles.elevatedButtonStyle,
+                      onPressed: () {},
+                      child: Text('Hastalık Detayı'),
+                    ),
+                    SizedBox(width: 16),
+                    SizedBox(height: 120, width: 150,
+                      child: ElevatedButton(
+                        style: ButtonStyles.elevatedButtonStyle,
+                        onPressed: () {
+                          
+                        },
+                        child: Text('Profili Düzenle'),
+                      ),
+                    ),
+                  ],
+                ),
+                //*************************************************************************************************************************** 
+                  ],
+                ),
+                
+                
+              ),
             ),
           ],
         ),
