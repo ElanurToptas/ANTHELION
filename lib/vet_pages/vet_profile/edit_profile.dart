60adac9f7d5b5e53dd08ab36f2d5c83fe6b79e4b
retropol <mehmet.demirturk@ogr.sakarya.edu.tr>
2023-06-27 21:34:37 +0300

    ++

--- lib/vet_pages/vet_profile/edit_profile.dart
+++ lib/vet_pages/vet_profile/edit_profile.dart
@@ -3,21 +3,14 @@ import 'package:petcare/tasarim_UI/bottom_bar.dart';
 import 'package:petcare/main.dart';
 
 class EditVetProfile extends StatefulWidget {
-  final int selectedIndex;
-
-  EditVetProfile({required this.selectedIndex});
-
   @override
   _EditVetProfileState createState() => _EditVetProfileState();
 }
 
 class _EditVetProfileState extends State<EditVetProfile> {
-  late int _selectedIndex;
-
   @override
   void initState() {
     super.initState();
-    _selectedIndex = widget.selectedIndex;
   }
 
   @override
@@ -28,15 +21,6 @@ class _EditVetProfileState extends State<EditVetProfile> {
       ),
       body: Center(
         child: Text('Sayfa 1 içeriği'),
-      ),
-      bottomNavigationBar: BottomBar(
-        selectedIndex: _selectedIndex,
-        onTabSelected: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-          // Burada sayfa değişimi gerçekleştirebilirsiniz
-        },
       ),
     );
   }
