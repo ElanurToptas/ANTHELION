84184cf0566035a7d8534ba7f60f50e9106f8970
retropol <mehmet.demirturk@ogr.sakarya.edu.tr>
2023-07-08 15:23:10 +0300

    added disease page

--- lib/vet_pages/animal/disease_diagnosis.dart
+++ lib/vet_pages/animal/disease_diagnosis.dart
@@ -1,22 +1,145 @@
 ﻿import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class SingleButtonWidget extends StatefulWidget {
+class DiseaseDiagnosisPage extends StatefulWidget {
+  final String chipNumber;
+
+  DiseaseDiagnosisPage({required this.chipNumber});
+
   @override
-  _SingleButtonWidgetState createState() => _SingleButtonWidgetState();
+  _DiseaseDiagnosisPageState createState() => _DiseaseDiagnosisPageState();
 }
 
-class _SingleButtonWidgetState extends State<SingleButtonWidget> {
-  bool _buttonPressed = false;
+class _DiseaseDiagnosisPageState extends State<DiseaseDiagnosisPage> {
+  final TextEditingController _diseaseController = TextEditingController();
+  List<String> diseases = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getDiseases();
+  }
+
+  void getDiseases() async {
+    DocumentSnapshot documentSnapshot = await FirebaseFirestore.instance
+        .collection('Pets')
+        .doc(widget.chipNumber)
+        .get();
+
+    if (documentSnapshot.exists) {
+      setState(() {
+        final data = documentSnapshot.data()
+            as Map<String, dynamic>?; // Dönüşümü gerçekleştirin
+        diseases = List<String>.from(data?['diseases'] ?? []);
+      });
+    }
+  }
+
+  void addDisease() {
+    String newDisease = _diseaseController.text.trim();
+    if (newDisease.isNotEmpty) {
+      setState(() {
+        diseases.add(newDisease);
+      });
+
+      FirebaseFirestore.instance
+          .collection('Pets')
+          .doc(widget.chipNumber)
+          .update({
+        'diseases': FieldValue.arrayUnion([newDisease])
+      });
+
+      _diseaseController.clear();
+    }
+  }
+
+  void removeDisease(String disease) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Hastalığı Sil'),
+          content: Text('Bu hastalığı silmek istediğinize emin misiniz?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Pencereyi kapat
+              },
+              child: Text('İptal'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  diseases.remove(disease);
+                });
+
+                FirebaseFirestore.instance
+                    .collection('Pets')
+                    .doc(widget.chipNumber)
+                    .update({
+                  'diseases': FieldValue.arrayRemove([disease])
+                });
+
+                Navigator.of(context).pop(); // Pencereyi kapat
+              },
+              child: Text('Sil'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _diseaseController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        setState(() {
-          _buttonPressed = !_buttonPressed;
-        });
-      },
-      child: Text(_buttonPressed ? 'Basıldı' : 'Bas'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hastalık Teşhisi'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Text(
+              'Seçilen Hayvanın Çip Numarası: ${widget.chipNumber}',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _diseaseController,
+              decoration: InputDecoration(
+                labelText: 'Hastalık',
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: addDisease,
+              child: Text('Hastalık Ekle'),
+            ),
+            SizedBox(height: 20),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: diseases.length,
+              itemBuilder: (context, index) {
+                final disease = diseases[index];
+                return ListTile(
+                  title: Text(disease),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () => removeDisease(disease),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/vet_pages/animal/lab_results.dart
+++ lib/vet_pages/animal/lab_results.dart
@@ -1,1 +1,92 @@
-﻿
+﻿import 'package:flutter/material.dart';
+import 'package:petcare/tasarim_UI/tema.dart';
+import 'package:petcare/vet_pages/vet_profile/edit_profile.dart';
+import 'package:petcare/vet_pages/animal/vet._animal.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ButtonStyles {
+  static final elevatedButtonStyle = ElevatedButton.styleFrom(
+    backgroundColor: Color.fromARGB(255, 111, 132, 255),
+    textStyle: TextStyle(fontSize: 12),
+    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+    minimumSize: Size(10, 10),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(8),
+    ),
+  );
+}
+
+class ChipUpdatePage extends StatelessWidget {
+  const ChipUpdatePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String chipNumber = '';
+
+    void updateChipNumber(String value) {
+      chipNumber = value;
+    }
+
+    void getAnimalInformation() {
+      String documentId = 'Chip ID'; // Dökümanın tam adını burada belirtin
+      FirebaseFirestore.instance
+          .collection('Pets')
+          .doc(chipNumber)
+          .get()
+          .then((DocumentSnapshot documentSnapshot) {
+        if (documentSnapshot.exists) {
+          // Hayvanın bilgileri başarıyla alındı, işlemleri burada gerçekleştirin
+          var animalData = documentSnapshot.data();
+          // Animal verilerini kullanarak yapmak istediğiniz işlemleri burada yapabilirsiniz
+        } else {
+          // Çip numarasına sahip hayvan bulunamadı, hata mesajı gösterebilirsiniz
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Hata'),
+                content: Text('Çip numarasına sahip hayvan bulunamadı.'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Tamam'),
+                  ),
+                ],
+              );
+            },
+          );
+        }
+      });
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hayvan Bilgilerini Getirme'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              onChanged: (value) => updateChipNumber(value),
+              decoration: InputDecoration(
+                labelText: 'Hayvan Çip No',
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              style: ButtonStyles.elevatedButtonStyle,
+              onPressed: () {
+                getAnimalInformation();
+              },
+              child: Text('Hayvan Bilgilerini Getir'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
