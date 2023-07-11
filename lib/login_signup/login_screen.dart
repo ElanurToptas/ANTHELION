15d277599424497487a94258d47205f3a7a33c00
Fatma Eser <fatmaeserr@gmail.com>
2023-07-11 11:46:14 +0300

    Tema_ Eksik_ Giderildi_ Tüm_ Sayfalara_Eklendi

--- lib/login_signup/login_screen.dart
+++ lib/login_signup/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:petcare/login_signup/password_reset_screen.dart';
 import 'package:petcare/login_signup/signup_page.dart';
+import 'package:petcare/tasarim_UI/tema.dart';
 import 'package:petcare/user_profile_pages/user_profile.dart';
 import '../vet_pages/vet._main.dart';
 
@@ -64,72 +65,75 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-     
-      body: _isLoginSuccess
-          ? _buildContent()
-          : Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextField(
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                      labelText: 'E-posta',
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    controller: _passwordController,
-                    decoration: InputDecoration(
-                      labelText: 'Şifre',
-                    ),
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 16.0),
-                  ElevatedButton(
-                    onPressed: _signInWithEmailAndPassword,
-                    child: Text('Giriş Yap'),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: ((context) {
-                              return PasswordResetPage();
-                            })),
-                          );
-                        },
-                        child: Text("Şifremi Unuttum"),
+    return MaterialApp(
+      theme: theme(),
+      home: Scaffold(
+       
+        body: _isLoginSuccess
+            ? _buildContent()
+            : Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        labelText: 'E-posta',
                       ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Hala üye değil misiniz ?"),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: ((context) {
-                              return SignUpScreen();
-                            })),
-                          );
-                        },
-                        child: Text("Hemen üye ol"),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        labelText: 'Şifre',
                       ),
-                    ],
-                  ),
-                ],
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 16.0),
+                    ElevatedButton(
+                      onPressed: _signInWithEmailAndPassword,
+                      child: Text('Giriş Yap'),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: ((context) {
+                                return PasswordResetPage();
+                              })),
+                            );
+                          },
+                          child: Text("Şifremi Unuttum"),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Hala üye değil misiniz ?"),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: ((context) {
+                                return SignUpScreen();
+                              })),
+                            );
+                          },
+                          child: Text("Hemen üye ol"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
+      ),
     );
   }
 
