334e7c49d52036cef9aec07c2c35a99803059af5
Fatma Eser <fatmaeserr@gmail.com>
2023-07-12 22:32:46 +0300

    Hizmet Sayfası Arama Kısmı Tamam

--- lib/tasarim_UI/services_page.dart
+++ lib/tasarim_UI/services_page.dart
@@ -30,17 +30,20 @@ class ServicesPage extends StatelessWidget {
   final TextEditingController _searchController = TextEditingController();
   final List<String> names = [
     'Pet Paradise Oteli',
-    'Collie',
-    'Golden Retriever',
-    'Chartreux',
-    'European Shorthair',
-    'Scottish Fold',
-    'Çin Hamster',
-    'Avrupa Hamster',
-    'Gonzales Hamster',
-    "Hothot Tavşanı",
-    "Hollanda Lop Tavşanı",
-    "Havana Tavşanı",
+    'Paws & Claws Evcil Hayvan Spa ve Tatil Köyü',
+    'Happy Tails Evcil Hayvan Resort',
+    'Ayşe Yılmaz',
+    'Ramazan Kaya',
+    'Zeynep Acar',
+    'Evcil Cennet',
+    'Patili Dükkan',
+    'Sevimli Köşe',
+    "Pampered Paws",
+    "Furry Styles",
+    "Paws & Co.",
+    "Pawsitive Pups",
+    "Canine Academy",
+    "Smart Paws Training Center"
   ];
 
   void _showCard(BuildContext context, MyCard card) {
@@ -160,152 +163,133 @@ class ServicesPage extends StatelessWidget {
                     );
                     _showCard(context, card);
                   }
-                  if (suggestion == "Collie") {
+                  if (suggestion == "Paws & Claws Evcil Hayvan Spa ve Tatil Köyü") {
                     MyCard card = MyCard(
-                      titles:["Kişilik & Karakter Özellikleri"],
-                      descriptions:[ "Collie zeki, tatlı, canlı, uysal, hassas, yumuşak huylu,"
-                          "uyumlu ve arkadaş canlısı bir köpektir. Collie sahibine sadık,"
-                      "eğitilmesi kolay ve itaatkar bir ırktır. Genellikle diğer evcil "
-                          "hayvanlarla ve diğer köpeklerle de arası iyi, arkadaş canlısıdır. "
-                          "Çocukları çok sever, çocuklarla oyun oynamaktan mutlu olurlar ve "
-                          "çocukları çevresindeki olumsuzluklara karşı korumak isterler. "
-                          "İyi yetiştirilmiş, eğitilmiş bir Collie köpeğinin oldukça arkadaş canlısı "
-                          "ve uysal olduğu gözlemlenmektedir. Collie karakter yapısıyla tam bir"
-                          " aile köpeğidir ve tüm ailenin vazgeçilmez bir parçası olmaktan çok mutlu olur.",],
+                      titles:["Evcil dostlarınıza şımartıcı bir kaçamak"],
+                      descriptions:[ "Paws & Claws Oteli, evcil hayvanınızın tatilini unutulmaz kılmak için lüks konaklama seçenekleri, spa hizmetleri ve eğlenceli aktiviteler sunar."
+"Profesyonel veterinerler ve bakım uzmanları, evcil dostlarınızın sağlık ve mutluluğu için özenle ilgilenir."
+"Otelimizde evcil hayvanlarınız için özel yüzme havuzları, oyun alanları ve doğa yürüyüş parkurları bulunur.",],
                       imagePath: 'asset/pictures-2/collie.png',
                     );
                     _showCard(context, card);
                   }
 
-                  if (suggestion == "Golden Retriever") {
+                  if (suggestion == "Happy Tails Evcil Hayvan Resort") {
                     MyCard card = MyCard(
-                      titles:["Kişilik & Karakter Özellikleri",],
-                      descriptions:[" Golden Retriever bulunduğu ortamda sevimliliği, cana yakınlığı ve oyunculuğu ile "
-                          "tüm gözleri üzerine çekmektedir. Orta büyüklüğe sahip bu sevimli ırk, "
-                          "açık kahverengi ve krem tonlarındaki yumuşak ve göz alıcı bedeniyle kalpleri kazanır."
-                          " Atletik, ve güçlü bir yapısı vardır. Güçlü ön ve arka ayakları ile karada olduğu gibi"
-                          " suda da hareket etme, avlanma yeteneğine sahiptir. Düz veya dalgalı kabarık kürke"
-                          " sahiptir. Dost canlısı olan Golden Retriever kısa sürede ailenizden birisi olur. ",],
+                      titles:["Mutlu anılar için mükemmel tatil mekan",],
+                      descriptions:[" Happy Tails Resort, evcil dostlarınızı ağırlarken onlara sevgi, güven ve eğlence dolu bir ortam sunar."
+"Geniş ve rahat konaklama birimlerimizde evcil hayvanlarınız kendilerini evlerinde hissedecekler."
+"Otelimizde özel yürüyüş parkurları, oyun grupları ve veteriner hizmetleri gibi birçok olanak bulunur. ",],
                       imagePath:  'asset/pictures-2/goldenR.png',
                     );
                     _showCard(context, card);
                   }
 
-                  if (suggestion == "Chartreux") {
+                  if (suggestion == "Ayşe Yılmaz") {
                     MyCard card = MyCard(
-                      titles:["Kişilik & Karakter Özellikleri"],
-                      descriptions:["Chartreux cinsi kediler özellikle de son dönemlerde en çok "
-                          "sevilen evcil hayvanlar arasında yer alıyor. Yumuşacık "
-                          "duman rengi tüyleri ile sahiplenmek isteyenleri anında büyülüyor."
-                          " Ayrıca ilk bakışta fark edilecek güzellikte olan gözleri ile de"
-                          " insanları etkisi altına alıyor. Diğer evcil kedilerden farklı olarak "
-                          "Chartreux cinsleri genel olarak ilgiden hoşlansalar da size kendi "
-                          "ilgisini ilk başta pek göstermeyecek türde kedilerdir.",],
+                      titles:["Benimle Evcil Hayvanınız Güvende!"],
+                      descriptions:["Merhaba! Ben hayvan bakıcısı olarak uzun yıllardır hizmet veren biriyim. Evcil hayvanınıza sevgi, ilgi ve güvence sağlamak için buradayım."
+                      ""
+                      "Hizmetlerim:"
+                      "Evde veya evinizde evcil hayvanınıza göz kulak olmak."
+"Beslenme, yürüyüş ve egzersiz programı oluşturmak."
+"Tuvalet ihtiyaçlarını temizlemek ve hijyenik ortam sağlamak."
+"İlaç vermek ve tıbbi bakım sağlamak (gerektiğinde)."
+"Oyun oynamak, sevgi dolu etkileşimlerde bulunmak.",],
                       imagePath:'asset/pictures-2/chartreux.png',
                     );
                     _showCard(context, card);
                   }
 
-                  if (suggestion == "European Shorthair") {
+                  if (suggestion == "Ramazan Kaya") {
                     MyCard card = MyCard(
-                      titles:["Kişilik & Karakter Özellikleri"],
-                      descriptions:["Onu ilk gördüğünüzde renk ve desenlerinden dolayı Tekir veya Sarman kedisine benzetebilirsiniz."
-                          " Ancak o kendine has görünümü, mizacı ve davranışlarıyla bir Tekir veya Sarman kedisinden daha fazlasıdır."
-                          " Tüm kedi ırkları arasında en doğalı olan European Shorthair kedisi, binlerce yıl öncesinde "
-                          "Romalılar tarafından Avrupa’ya getirilmiş melez bir ırktır.",],
+                      titles:["Profesyonel Evcil Hayvan Bakıcısı"],
+                      descriptions:["Merhaba! Hayvansever ve sorumluluk sahibi bir hayvan bakıcısıyım. Evcil hayvanınıza gereken sevgi, ilgi ve bakımı sağlamak için buradayım."
+                      ""
+                      "Hizmetlerim:"
+                      "Günlük beslenme, su değişimi ve tuvalet ihtiyaçlarını karşılamak."
+"Yürüyüş, egzersiz ve oyun programı oluşturmak."
+"Evcil hayvanınıza ilaç vermek ve tıbbi bakım sağlamak."
+"Evde veya evinizde güvenli bir ortam oluşturmak."
+"Evcil hayvanınıza sevgi dolu etkileşimlerde bulunmak.",],
                       imagePath:'asset/pictures-2/europeanS.png',
                     );
                     _showCard(context, card);
                   }
 
-                  if (suggestion == "Scottish Fold") {
+                  if (suggestion == "Zaynep Acar") {
                     MyCard card = MyCard(
-                      titles:["Kişilik & Karakter Özellikleri"],
-                      descriptions: ["İskoç kırması bu tatlı kediler, İskoçya’daki çiftlik kedilerinin kendiliğinden"
-                          " bir mutasyon sonucu ortaya çıkmış bir cinstir. En belirgin özellikleri "
-                          "kulaklarının katlı olmasıdır. Bu özelliğiyle farklı ve dayanıklı bir görünüme sahip olan"
-                          " Scottish Fold’lar aynı zamanda arkadaş canlısı ve yüksek zekâ seviyesine sahip türde birer cins kedidirler.",],
+                      titles:["Evcil Hayvan Bakımı için Profesyonel Yardım"],
+                      descriptions:["Merhaba! Hayvansever ve sorumluluk sahibi bir hayvan bakıcısıyım. Evcil hayvanınıza gereken sevgi, ilgi ve bakımı sağlamak için buradayım."
+                      ""
+                      "Hizmetlerim:"
+                      "Günlük beslenme, su değişimi ve tuvalet ihtiyaçlarını karşılamak."
+"Yürüyüş, egzersiz ve oyun programı oluşturmak."
+"Evcil hayvanınıza ilaç vermek ve tıbbi bakım sağlamak."
+"Evde veya evinizde güvenli bir ortam oluşturmak."
+"Evcil hayvanınıza sevgi dolu etkileşimlerde bulunmak.",],
                       imagePath:'asset/pictures-2/scottishF.png',
                     );
                     _showCard(context, card);
                   }
 
-                  if (suggestion == "Çin Hamster") {
+                  if (suggestion == "Evcil Cennet") {
                     MyCard card = MyCard(
-                      titles:["Kişilik & Karakter Özellikleri"],
-                      descriptions:["Çin çizgili hamster gerçek cüce hamster değildir, "
-                          "ancak diğer küçük hamsterlara benzer boyuttadır."
-                          " Adından da anlaşılacağı gibi, aslında Çin ve Moğolistan'dan."
-                          " Çin hamsterları yaygın olarak yetiştirilmez, evcil hayvan"
-                          " mağazalarında bulmak zor olabilir ve ayrıca onları tutmak için bir iznin "
-                          "gerekli olduğu Kaliforniya eyaleti gibi bazı yerlerde kısıtlıdır.",],
+                      titles:["Tüylü dostlarınızın cenneti!"],
+                      descriptions:["Köpek maması, kedi kumu, oyuncaklar, tasma ve tasarım evcil hayvan eşyaları.",],
                       imagePath:'asset/pictures-2/çinH.png',
                     );
                     _showCard(context, card);
                   }
 
-                  if (suggestion == "Avrupa Hamster") {
+                  if (suggestion == "Patili Dükkan") {
                     MyCard card = MyCard(
-                      titles:["Kişilik & Karakter Özellikleri"],
-                      descriptions:["Avrupa Hamsterı, 30 cm.’lik boyuyla Hamster türlerinin en büyüğüdür."
-                          " Renk farklılığı ile de diğer türlerden ayrılır. Karın bölgesi siyah,"
-                          " sırtı kahverengi ve yan bölgeler beyazdır. Doğu Avrupa ve"
-                          " Batı Asya’da toprağın altında yaşar. Tarım alanlarına büyük zararlar vermesiyle bilinir.",],
+                      titles:["Patilerinize özel bir alışveriş deneyimi!"],
+                      descriptions:["Köpek mamaları ve atıştırmalıklar, kedi kumu ve kedi maması, kuş yemleri ve kafesler, akvaryum malzemeleri.",],
                       imagePath: 'asset/pictures-2/avrupaH.png',
                     );
                     _showCard(context, card);
                   }
 
 
-                  if (suggestion == "Gonzales Hamster") {
+                  if (suggestion == "Sevimli Köşe") {
                     MyCard card = MyCard(
-                      titles:["Kişilik & Karakter Özellikleri"],
-                      descriptions:["Gonzales hamster doğada yarı kurak bozkırları mesken edinir."
-                          " Burada kazdığı yaklaşık 1 metre uzunluğundaki yuvasında yaşar."
-                          " Yuvasının duvarlarına koyun yünü ve kuru otlarla kaplar. "
-                          "Bu sayede dışarıdaki hava -25 santigrat derece iken, içindeki sıcaklık "
-                          "15 santigrat derece civarında olur. Bu sıcaklık gonzales hamsterın yavrularını "
-                          "yetiştirmesi için ideal değerdir.",],
+                      titles:["Sevgi dolu evcil hayvanlar için doğru adres!"],
+                      descriptions:["Köpek ve kedi mamaları, evcil hayvan oyuncakları, kuş kafesleri, balık tankları ve akvaryum malzemeleri.",],
                       imagePath:  'asset/pictures-2/gonzalesH.png',
                     );
                     _showCard(context, card);
                   }
 
-                  if (suggestion == "Hothot Tavşanı") {
+                  if (suggestion == "Pampered Paws") {
                     MyCard card = MyCard(
-                      titles:["Kişilik & Karakter Özellikleri"],
-                      descriptions:["Tür genel anlamda arkadaş canlısıdır ve evcil hayvan olarak beslenebilir. "
-                          "Sahiplenilen tavşanın sosyal bir canlı olması için yavruluğundan itibaren insanlarla"
-                          " etkileşim halinde olması gerekir. Günlük ihtiyaçlarının karşılanması için kafesinden"
-                          " çıkartılması iyi bir başlangıç olacaktır. Bu sayede insanlar tarafından tutulmaya alışır. "
-                          "Çocuklar tarafından beslenecekse, onlara tavşanı nasıl tutmaları gerektiği öğretilmelidir.",],
+                      titles:["Patileri şımartan bir dokunuş!"],
+                      descriptions:["Köpek ve kedi tımarı, tüy kesimi, yıkanma ve kurutma, tırnak kesimi, kulak temizliği, taraklama ve özel cilt bakımı.",],
                       imagePath:  "asset/pictures-2/Hothot tavşanı (2).png",
                     );
                     _showCard(context, card);
                   }
 
-                  if (suggestion == "Hollanda Lop Tavşanı") {
+                  if (suggestion == "Furry Styles") {
                     MyCard card = MyCard(
                       titles:["Kişilik & Karakter Özellikleri"],
-                      descriptions:[ "Hollanda lop tavşanınızın karakter gelişimi için, ona kafesinin "
-                          "dışında geçireceği uzun vakitler sağlamanız çok önemlidir. "
-                          "Evde yaşayan tavşanlar evin tamamında olmasa bile en azından bir odasında "
-                          "kafesinden çıkıp etrafta dolaşabilmeli, bacaklarını esnetebilmeli, güneş ışığı almalı"
-                          " ve özellikle sahibi ile etkileşim halinde olabilmelidir. Bu ufak tavşanlar ister yalnız "
-                          "yaşayan bir insan, ister evli bir çift, isterse de çocuklu aileler olsun; herkes için harika"
-                          " evcil hayvanlardır.Hollanda lopları görece aktif tavşanlardır ve hava sıcaklığı uygun "
-                          "olduğunda dışarıda vakit geçirmeye bayılırlar. Etrafı çevrili bir bahçe onlar için"
-                          " harika olacaktır, ancak açık bir bahçede kafes içinde beslenmelidir. "
-                          "Üstü açık, çevresi tel örgüyle çevrili bir kafes hem tavşanınıza biraz özgürlük "
-                          "hissi verecek, hem de komşu bahçelere kaçmasını önleyecektir.",],
+                      descriptions:[ "Tüy kesimi ve şekillendirme, yaratıcı tımar stilleri, renkli tüy boyama, köpük banyolar, tırnak cilalama, göz ve kulak temizliği.",],
                       imagePath:  'asset/pictures-2/Hollanda Lop tavşanı (2).png',
                     );
                     _showCard(context, card);
                   }
 
-                  if (suggestion == "Havana Tavşanı") {
+                  if (suggestion == "Paws & Co.") {
                     MyCard card = MyCard(
-                      titles:["Kişilik & Karakter Özellikleri"],
+                      titles:["Sağlıklı tüyler, mutlu patiler!"],
+                      descriptions:[ "Tüy kesimi, yıkanma ve kurutma, fırçalama, tırnak bakımı, koku giderme, cilt ve tüy bakımı, spa tedavileri, özel tımar paketleri.",],
+                      imagePath: 'asset/pictures-2/Havana tavşanı (2).png',
+                    );
+                    _showCard(context, card);
+                  }
+                  if (suggestion == "Pawsitive Pups") {
+                    MyCard card = MyCard(
+                      titles:["Köpeğinizle pozitif bir bağ kurun!"],
                       descriptions:[ "Bazı tavşanlar birçok oyuncakla eğlenmeye ihtiyaç duyarlar "
                           "(bir mağaza satın alınmış mı yoksa tuvalet kağıdı rulosu kadar basit bir şey "
                           "tamamen size bağlıdır), diğerleri onları mutlu etmek için fazla bir şeye ihtiyaç duymaz."
@@ -317,6 +301,22 @@ class ServicesPage extends StatelessWidget {
                           "Birçok evcil hayvan ailesi, evin etrafına birkaç çöp kutusu yerleştirmenin "
                           "en iyi şekilde işe yaradığını fark etti, çünkü tavşanı yapmak için tavşanınızın "
                           "evinizin diğer tarafına gitmesi gerekmiyor.",],
+                      imagePath: 'asset/pictures-2/Havana tavşanı (2).png',
+                    );
+                    _showCard(context, card);
+                  }
+                  if (suggestion == "Cannie Academy") {
+                    MyCard card = MyCard(
+                      titles:["Köpeğinizi eğitirken en iyiyi arayın!"],
+                      descriptions:[ "Temel komut eğitimi, problem davranış düzeltme, köpek davranış analizi, sosyalizasyon, çevre uyum eğitimi, terapi köpeği eğitimi.",],
+                      imagePath: 'asset/pictures-2/Havana tavşanı (2).png',
+                    );
+                    _showCard(context, card);
+                  }
+                  if (suggestion == "Smart Paws Training Center") {
+                    MyCard card = MyCard(
+                      titles:["Zekice eğitilmiş patiler!"],
+                      descriptions:[ "Temel itaat eğitimi, yürüyüş eğitimi, sosyalleşme, problem davranışların düzeltilmesi, ileri itaat eğitimi, köpek sporları eğitimi.",],
                       imagePath: 'asset/pictures-2/Havana tavşanı (2).png',
                     );
                     _showCard(context, card);
