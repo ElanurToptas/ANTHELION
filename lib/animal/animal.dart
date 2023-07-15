9b240b88e84bec07a8e6bea7c0dd9da2aac073ee
Coders Club <hi@codersclub.co>
2023-07-16 02:00:06 +0300

    Hizmet_sayfası_bitti

--- lib/animal/animal.dart
+++ lib/animal/animal.dart
@@ -95,71 +95,148 @@ class HomePage extends StatelessWidget {
       imagePath: 'asset/pictures-2/goldenR.png',
     ),
       MyCard(
-  titles:["Kişilik & Karakter Özellikleri"],
+  titles:["Kişilik & Karakter Özellikleri","----------------------------","Sağlık Durumu","Hipertrofik Kardiyomiyopati (HCM)","Polikistik Böbrek Hastalığı","Diş Problemleri","Obezite"],
   descriptions:["Chartreux cinsi kediler özellikle de son dönemlerde en çok "
   "sevilen evcil hayvanlar arasında yer alıyor. Yumuşacık "
   "duman rengi tüyleri ile sahiplenmek isteyenleri anında büyülüyor."
   " Ayrıca ilk bakışta fark edilecek güzellikte olan gözleri ile de"
   " insanları etkisi altına alıyor. Diğer evcil kedilerden farklı olarak "
   "Chartreux cinsleri genel olarak ilgiden hoşlansalar da size kendi "
-  "ilgisini ilk başta pek göstermeyecek türde kedilerdir.",],
+  "ilgisini ilk başta pek göstermeyecek türde kedilerdir.",
+    "",
+    "Chartreux kedileri genellikle sağlıklı ve dayanıklı kedilerdir. Ancak, her türün olduğu gibi bazı genetik sağlık sorunları veya hastalıklarla karşılaşabilirler."
+        " Kedilerin sağlık durumu, genetik yatkınlıklar, yaşam tarzı, beslenme ve çevresel faktörlere bağlı olarak değişebilir.",
+    " Kalp kasının kalınlaşmasıyla karakterize edilen bir kalp hastalığıdır. Irksal olarak bazı kedilerde daha yaygın olabilir.",
+    "Böbreklerde sıvı dolu kistlerin oluştuğu bir durumdur. Bazı kedilerde genetik olarak görülebilir.",
+    "Kedilerde diş taşı birikimi, diş eti hastalıkları ve diğer diş sorunları yaygın olabilir. Düzenli diş bakımı bu tür sorunların önlenmesine yardımcı olabilir.",
+    "Kısırlaştırılmış veya kısırlaştırılmamış kedilerde obezite eğilimi olabilir. Sağlıklı bir diyet ve düzenli egzersiz, ideal vücut ağırlığını korumalarına yardımcı olur.",],
   imagePath:'asset/pictures-2/chartreux.png',
   ),
+
   MyCard(
-  titles:["Kişilik & Karakter Özellikleri"],
+  titles:["Kişilik & Karakter Özellikleri","----------------------------","Sağlık Durumu","Feline Üst Solunum Yolu Enfeksiyonlar","Sindirim Problemleri","Diş Sorunları","Kardiyomiyopati"],
   descriptions:["Onu ilk gördüğünüzde renk ve desenlerinden dolayı Tekir veya Sarman kedisine benzetebilirsiniz."
   " Ancak o kendine has görünümü, mizacı ve davranışlarıyla bir Tekir veya Sarman kedisinden daha fazlasıdır."
   " Tüm kedi ırkları arasında en doğalı olan European Shorthair kedisi, binlerce yıl öncesinde "
-  "Romalılar tarafından Avrupa’ya getirilmiş melez bir ırktır.",],
+  "Romalılar tarafından Avrupa’ya getirilmiş melez bir ırktır.",
+    "",
+    "Avrupa Kırmızısı (European Shorthair), doğal olarak ortaya çıkan ve kökeni Avrupa'ya dayanan bir kedidir."
+        " Diğer kedi ırklarına göre daha az tanınır, ancak sağlıklı ve dayanıklı bir kedi türü olarak bilinir. Bu nedenle, çoğunlukla genetik sağlık sorunlarına yatkınlığı düşük olabilir.",
+    "Virüsler veya bakteriler nedeniyle solunum yollarını etkileyen yaygın bir hastalıktır.",
+    "Kusma ve ishal gibi sindirim rahatsızlıkları bazı kedi ırklarında görülebilir ve Avrupa Kırmızısı bu durumdan muaf değildir.",
+    "Kedilerde diş taşı birikimi, diş eti hastalıkları ve diğer diş problemleri yaşlanma süreciyle birlikte ortaya çıkabilir.",
+    "Kalp hastalığı, bazı kedilerde genetik olarak görülebilir ve Avrupa Kırmızısı bu türden etkilenebilir.",],
   imagePath:'asset/pictures-2/europeanS.png',
   ),
   MyCard(
-  titles:["Kişilik & Karakter Özellikleri"],
+  titles:["Kişilik & Karakter Özellikleri","----------------------------","Sağlık Durumu","Kıkırdak Sorunları", "Osteokondrodisplazi", "Polikistik Böbrek Hastalığı", "Diş Problemleri",],
   descriptions: ["İskoç kırması bu tatlı kediler, İskoçya’daki çiftlik kedilerinin kendiliğinden"
   " bir mutasyon sonucu ortaya çıkmış bir cinstir. En belirgin özellikleri "
   "kulaklarının katlı olmasıdır. Bu özelliğiyle farklı ve dayanıklı bir görünüme sahip olan"
-  " Scottish Fold’lar aynı zamanda arkadaş canlısı ve yüksek zekâ seviyesine sahip türde birer cins kedidirler.",],
+  " Scottish Fold’lar aynı zamanda arkadaş canlısı ve yüksek zekâ seviyesine sahip türde birer cins kedidirler.",
+    "",
+    "İskoç Kısa Tüylü (Scottish Fold) kedileri, genellikle sağlıklı ve dayanıklı kediler olarak bilinir."
+        "Bu türün en belirgin özelliği, kulaklarının uçlarındaki kıkırdakların katlanması nedeniyle kulaklarının aşağı doğru katlanmış görünümüdür. "
+        "Ancak, bu genetik özellikleri bazı sağlık sorunlarına yatkınlığı da beraberinde getirebilir.",
+    " Kulak katlaması, bazı Scottish Fold kedilerinde kıkırdak ve eklem sorunlarına neden olabilir. Bu, yaşam kalitesini etkileyebilecek eklem sertleşmesi veya ağrısına yol açabilir.",
+    " Bu durum, kıkırdak ve kemik gelişimine etki ederek eklemlerde ağrı ve hareket kısıtlılığına neden olabilir.",
+    "Bazı Scottish Fold kedileri, böbreklerde kistlerin oluştuğu polikistik böbrek hastalığına yatkın olabilir.",
+    " Kedilerde yaygın olarak görülen diş taşı birikimi, diş eti rahatsızlıkları ve diğer diş sorunları Scottish Fold kedilerini de etkileyebilir.",],
   imagePath:'asset/pictures-2/scottishF.png',
   ),
+
   MyCard(
-  titles:["Kişilik & Karakter Özellikleri"],
+  titles:["Kişilik & Karakter Özellikleri","----------------------------","Sağlık Durumu","Sindirim Problemleri","Solunum Yolu Enfeksiyonları","Cilt Problemleri","Polikistik Böbrek Hastalığı","Diş Problemleri","Osteokondrodisplazi",],
   descriptions:["Çin çizgili hamster gerçek cüce hamster değildir, "
   "ancak diğer küçük hamsterlara benzer boyuttadır."
   " Adından da anlaşılacağı gibi, aslında Çin ve Moğolistan'dan."
   " Çin hamsterları yaygın olarak yetiştirilmez, evcil hayvan"
   " mağazalarında bulmak zor olabilir ve ayrıca onları tutmak için bir iznin "
-  "gerekli olduğu Kaliforniya eyaleti gibi bazı yerlerde kısıtlıdır.",],
+  "gerekli olduğu Kaliforniya eyaleti gibi bazı yerlerde kısıtlıdır.",
+    "",
+    "Çin hamsterları, genellikle sağlıklı ve dayanıklı kemirgenlerdir."
+        " Ancak, tüm evcil hayvanlar gibi, onların da sağlıklarını korumak için özenli bir bakıma ve dikkatli bir gözlemeye ihtiyaçları vardır. ",
+    "Çin hamsterları bazen kabızlık veya ishal yaşayabilir. Yanlış beslenme, kirli içme suyu ve stres gibi faktörler sindirim sorunlarına neden olabilir.",
+    "Soğuk ve nemli ortamlarda tutulduklarında veya maruz kaldıklarında, solunum yolu enfeksiyonları gelişebilir. Hapşırma, burun akıntısı ve hırıltılı solunum gibi belirtiler gösterebilirler.",
+    "Parazitler veya bakteriyel enfeksiyonlar nedeniyle cilt rahatsızlıkları yaşayabilirler. Kaşıntı, kızarıklık veya kabuklanma gibi belirtiler görülebilir.",
+    "Çin hamsterlarında böbreklerde sıvı dolu kistlerin oluştuğu bir durum olabilir. Bu, böbrek fonksiyonunu etkileyebilir.",
+    "Dişlerin aşırı büyümesi, yeme güçlüğüne ve diş eti problemlerine yol açabilir.",
+    "Bu durumda, kıkırdak ve kemik gelişiminde sorunlar yaşanarak eklem ağrısı ve hareket kısıtlılığı ortaya çıkabilir.",],
   imagePath:'asset/pictures-2/çinH.png',
   ),
   MyCard(
-  titles:["Kişilik & Karakter Özellikleri"],
+  titles:["Kişilik & Karakter Özellikleri","----------------------------","Sağlık Durumu",
+    "Sindirim Problemleri",
+    "Cilt Problemleri",
+    "Diş Problemleri",
+    "Stres ve Sosyal İzolasyon",],
   descriptions:["Avrupa Hamsterı, 30 cm.’lik boyuyla Hamster türlerinin en büyüğüdür."
   " Renk farklılığı ile de diğer türlerden ayrılır. Karın bölgesi siyah,"
   " sırtı kahverengi ve yan bölgeler beyazdır. Doğu Avrupa ve"
-  " Batı Asya’da toprağın altında yaşar. Tarım alanlarına büyük zararlar vermesiyle bilinir.",],
+  " Batı Asya’da toprağın altında yaşar. Tarım alanlarına büyük zararlar vermesiyle bilinir.",
+
+    "",
+    "Avrupa hamsterı (Cricetus cricetus), genellikle sağlıklı ve dayanıklı kemirgenlerdir."
+        " Ancak, evcil hayvan olarak beslenmeleri durumunda da bazı sağlık sorunları ortaya çıkabilir. ",
+    "Yanlış beslenme, stres veya kirli içme suyu gibi faktörler sindirim sorunlarına yol açabilir.",
+    "Parazitler veya bakteriyel enfeksiyonlar nedeniyle cilt rahatsızlıkları yaşanabilir.",
+    "Aşırı diş büyümesi, yeme güçlüğüne ve diş eti problemlerine neden olabilir.",
+    "Uygun yaşam alanı ve aktiviteler sunulmazsa stres yaşayabilirler.",],
   imagePath: 'asset/pictures-2/avrupaH.png',
   ),
+
   MyCard(
-  titles:["Kişilik & Karakter Özellikleri"],
+  titles:["Kişilik & Karakter Özellikleri","----------------------------","Sağlık Durumu",
+    "Sindirim Problemleri",
+    "Diş Problemleri",
+    "Cilt Problemleri",
+    "Stres ve Sosyal İzolasyon",],
   descriptions:["Gonzales hamster doğada yarı kurak bozkırları mesken edinir."
   " Burada kazdığı yaklaşık 1 metre uzunluğundaki yuvasında yaşar."
   " Yuvasının duvarlarına koyun yünü ve kuru otlarla kaplar. "
   "Bu sayede dışarıdaki hava -25 santigrat derece iken, içindeki sıcaklık "
   "15 santigrat derece civarında olur. Bu sıcaklık gonzales hamsterın yavrularını "
-  "yetiştirmesi için ideal değerdir.",],
+  "yetiştirmesi için ideal değerdir.",
+    "",
+    "Gonzales hamsterı, hamster türlerinden biri olan bir evcil hayvandır."
+        " Sağlıklı bir Gonzales hamsterı, genellikle aktif, canlı ve iyi beslenmiş olacaktır."
+        " Ancak, her türün olduğu gibi, bazı sağlık sorunları ortaya çıkabilir.",
+    "Yanlış beslenme veya stres, kabızlık veya ishal gibi sindirim sorunlarına neden olabilir.",
+    "Aşırı diş büyümesi, yeme güçlüğüne ve diş eti sorunlarına yol açabilir.",
+    "Parazitler veya bakteriyel enfeksiyonlar, cilt rahatsızlıklarına neden olabilir.",
+    "Gonzales hamsterları sosyal hayvanlar değildir, ancak uygun yaşam alanı ve aktiviteler sunulmazsa stres yaşayabilirler.",],
   imagePath:  'asset/pictures-2/gonzalesH.png',
   ),
+
   MyCard(
-  titles:["Kişilik & Karakter Özellikleri"],
+  titles:["Kişilik & Karakter Özellikleri","----------------------------","Sağlık Durumu",
+    "Diş Problemleri",
+    "Sindirim Sorunları",
+    "Cilt Parazitleri",
+    "Cilt Parazitleri",
+    "Dış Parazitler",],
   descriptions:["Tür genel anlamda arkadaş canlısıdır ve evcil hayvan olarak beslenebilir. "
   "Sahiplenilen tavşanın sosyal bir canlı olması için yavruluğundan itibaren insanlarla"
   " etkileşim halinde olması gerekir. Günlük ihtiyaçlarının karşılanması için kafesinden"
   " çıkartılması iyi bir başlangıç olacaktır. Bu sayede insanlar tarafından tutulmaya alışır. "
-  "Çocuklar tarafından beslenecekse, onlara tavşanı nasıl tutmaları gerektiği öğretilmelidir.",],
+  "Çocuklar tarafından beslenecekse, onlara tavşanı nasıl tutmaları gerektiği öğretilmelidir.",
+    "",
+    "Bu cins genellikle sağlıklı ve dayanıklı tavşanlar olarak bilinir.",
+    "Sürekli büyüyen dişler nedeniyle diş sorunları yaşanabilir.",
+    "Yanlış beslenme veya stres, kabızlık veya ishal gibi sindirim rahatsızlıklarına yol açabilir.",
+    " Parazitler, tüy ve cilt sağlığını etkileyebilir.",
+    "Soğuk ve nemli ortamlarda solunum yolu enfeksiyonları gelişebilir.",
+  "Pire ve keneler gibi dış parazitlerin kontrol edilmesi önemlidir."],
   imagePath:  "asset/pictures-2/Hothot tavşanı (2).png",
   ),
+
   MyCard(
-  titles:["Kişilik & Karakter Özellikleri"],
+  titles:["Kişilik & Karakter Özellikleri","----------------------------","Sağlık Durumu",
+    "Beslenme",
+    "Diş Sağlığı",
+    "Hareket ve Aktivite",
+    "Cilt ve Tüy Sağlığı",
+    "Sosyal İhtiyaçlar",],
   descriptions:[ "Hollanda lop tavşanınızın karakter gelişimi için, ona kafesinin "
   "dışında geçireceği uzun vakitler sağlamanız çok önemlidir. "
   "Evde yaşayan tavşanlar evin tamamında olmasa bile en azından bir odasında "
@@ -170,11 +247,24 @@ class HomePage extends StatelessWidget {
   "olduğunda dışarıda vakit geçirmeye bayılırlar. Etrafı çevrili bir bahçe onlar için"
   " harika olacaktır, ancak açık bir bahçede kafes içinde beslenmelidir. "
   "Üstü açık, çevresi tel örgüyle çevrili bir kafes hem tavşanınıza biraz özgürlük "
-  "hissi verecek, hem de komşu bahçelere kaçmasını önleyecektir.",],
+  "hissi verecek, hem de komşu bahçelere kaçmasını önleyecektir.",
+    "",
+    "Hollanda Lop tavşanları, genellikle sağlıklı ve dayanıklı tavşanlardır."
+        " Ancak, tüm tavşan cinslerinde olduğu gibi, bazı sağlık sorunlarına yatkınlıkları olabilir",
+    "Hollanda Lop tavşanları için dengeli ve sağlıklı bir beslenme sağlamak önemlidir."
+        " Kaliteli tavşan yemi, taze meyve, sebze ve saman vermek, sağlıklarını korumak için gereklidir.",
+    "Tavşanlar sürekli büyüyen dişlere sahiptir, bu nedenle diş bakımı önemlidir."
+        " Çiğneme materyalleri ve lifli yiyecekler vererek dişlerinin düzgün uzamasına yardımcı olunmalıdır.",
+    " Hollanda Lop tavşanları aktif ve hareketli hayvanlardır. "
+        "Uygun yaşam alanı ve düzenli egzersiz, fiziksel sağlıklarını destekler.",
+    "Tavşanların tüyleri ve ciltleri düzenli olarak kontrol edilmeli, parazitlerden ve diğer cilt sorunlarından korunmalıdır.",
+  "Tavşanlar sosyal hayvanlardır, bu nedenle sosyal etkileşim ve oyun zamanı sağlanmalıdır."],
   imagePath:  'asset/pictures-2/Hollanda Lop tavşanı (2).png',
   ),
+
   MyCard(
-  titles:["Kişilik & Karakter Özellikleri"],
+  titles:["Kişilik & Karakter Özellikleri","----------------------------","Sağlık Durumu",
+    ],
   descriptions:[ "Bazı tavşanlar birçok oyuncakla eğlenmeye ihtiyaç duyarlar "
   "(bir mağaza satın alınmış mı yoksa tuvalet kağıdı rulosu kadar basit bir şey "
   "tamamen size bağlıdır), diğerleri onları mutlu etmek için fazla bir şeye ihtiyaç duymaz."
@@ -185,7 +275,11 @@ class HomePage extends StatelessWidget {
   " ama diğer hayvanlara göre daha fazla sabır ve zaman gerektiriyorlar. "
   "Birçok evcil hayvan ailesi, evin etrafına birkaç çöp kutusu yerleştirmenin "
   "en iyi şekilde işe yaradığını fark etti, çünkü tavşanı yapmak için tavşanınızın "
-  "evinizin diğer tarafına gitmesi gerekmiyor.",],
+  "evinizin diğer tarafına gitmesi gerekmiyor.",
+    "",
+    "Havana tavşanları, genellikle sağlıklı ve dayanıklı hayvanlar olarak bilinir."
+        " Sağlıklı bir Havana tavşanı için dengeli bir beslenme, temiz bir yaşam ortamı ve düzenli veteriner kontrolleri önemlidir."
+        " Egzersiz sağlanmalı, cilt ve tüy bakımına dikkat edilmelidir.",],
   imagePath: 'asset/pictures-2/Havana tavşanı (2).png',
   ),
 
