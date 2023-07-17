85a9004fbc648543fd8b1a4463a2a6d3ad61cd3b
retropol <mehmet.demirturk@ogr.sakarya.edu.tr>
2023-07-17 10:24:12 +0300

    bug_fixed

--- lib/login_signup/login_screen.dart
+++ lib/login_signup/login_screen.dart
@@ -19,6 +19,7 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   late User? _user;
+  bool _isLoginSuccess = false;
 
   @override
   void initState() {
@@ -30,22 +31,27 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   void _checkUserType(String uid) async {
-    DocumentSnapshot userSnapshot =
-        await FirebaseFirestore.instance.collection('Users').doc(uid).get();
-    DocumentSnapshot vetSnapshot = await FirebaseFirestore.instance
-        .collection('Veterinarians')
-        .doc(uid)
-        .get();
-    if (userSnapshot.exists && userSnapshot.get('isUser') == 'true') {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => UserProfile()),
-      );
-    } else if (vetSnapshot.exists && vetSnapshot.get('isVet') == 'true') {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => ProfilePage()),
-      );
+    try {
+      DocumentSnapshot userSnapshot =
+          await FirebaseFirestore.instance.collection('Users').doc(uid).get();
+      DocumentSnapshot vetSnapshot = await FirebaseFirestore.instance
+          .collection('Veterinarians')
+          .doc(uid)
+          .get();
+      if (mounted) {
+        if (userSnapshot.exists && userSnapshot.get('isUser') == 'true') {
+          setState(() {
+            _isLoginSuccess = true;
+          });
+        } else if (vetSnapshot.exists && vetSnapshot.get('isVet') == 'true') {
+          setState(() {
+            _isLoginSuccess = true;
+          });
+        }
+      }
+    } catch (e) {
+      // Handle any potential errors
+      print('Error: $e');
     }
   }
 
@@ -86,96 +92,155 @@ class _LoginScreenState extends State<LoginScreen> {
                 fit: BoxFit.cover,
               ),
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'asset/Kullanıcı/uye.png',
-                  ),
-                  TextField(
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                      labelText: 'E-posta',
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    controller: _passwordController,
-                    decoration: InputDecoration(
-                      labelText: 'Şifre',
-                    ),
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 16.0),
-                  ElevatedButton(
-                    onPressed: _signInWithEmailAndPassword,
-                    child: Text(
-                      'Giriş Yap',
-                      style: TextStyle(
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: ((context) {
-                              return PasswordResetPage();
-                            })),
-                          );
-                        },
-                        child: Text(
-                          "Şifremi Unuttum!",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.indigo,
+            child: _isLoginSuccess
+                ? _buildContent()
+                : Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'asset/Kullanıcı/uye.png',
+                        ),
+                        TextField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            labelText: 'E-posta',
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Hala üye değil misiniz ?",
-                        style: TextStyle(
-                          fontSize: 18,
+                        SizedBox(
+                          height: 10,
                         ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: ((context) {
-                              return SignUpScreen();
-                            })),
-                          );
-                        },
-                        child: Text(
-                          "Hemen üye ol!",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.indigo,
+                        TextField(
+                          controller: _passwordController,
+                          decoration: InputDecoration(
+                            labelText: 'Şifre',
+                          ),
+                          obscureText: true,
+                        ),
+                        SizedBox(height: 16.0),
+                        ElevatedButton(
+                          onPressed: _signInWithEmailAndPassword,
+                          child: Text(
+                            'Giriş Yap',
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: ((context) {
+                                    return PasswordResetPage();
+                                  })),
+                                );
+                              },
+                              child: Text(
+                                "Şifremi Unuttum!",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.indigo,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Hala üye değil misiniz ?",
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: ((context) {
+                                    return SignUpScreen();
+                                  })),
+                                );
+                              },
+                              child: Text(
+                                "Hemen üye ol!",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.indigo,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
-            ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildContent() {
+    if (_isLoginSuccess) {
+      if (_user != null) {
+        return FutureBuilder<DocumentSnapshot>(
+          future: FirebaseFirestore.instance
+              .collection('Users')
+              .doc(_user!.uid)
+              .get(),
+          builder: (BuildContext context,
+              AsyncSnapshot<DocumentSnapshot> userSnapshot) {
+            if (userSnapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator(); // Show a loading indicator while waiting for the snapshot
+            }
+            if (userSnapshot.hasError) {
+              return Text(
+                  'Error: ${userSnapshot.error}'); // Show an error message if snapshot retrieval fails
+            }
+            if (userSnapshot.data != null &&
+                userSnapshot.data!.exists &&
+                userSnapshot.data!.get('isUser') == 'true') {
+              return UserProfile();
+            } else {
+              return FutureBuilder<DocumentSnapshot>(
+                future: FirebaseFirestore.instance
+                    .collection('Veterinarians')
+                    .doc(_user!.uid)
+                    .get(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<DocumentSnapshot> vetSnapshot) {
+                  if (vetSnapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  }
+                  if (vetSnapshot.hasError) {
+                    return Text('Error: ${vetSnapshot.error}');
+                  }
+                  if (vetSnapshot.data != null &&
+                      vetSnapshot.data!.exists &&
+                      vetSnapshot.data!.get('isVet') == 'true') {
+                    return ProfilePage();
+                  } else {
+                    return Center(
+                      child: Text('Giriş Başarılı!'),
+                    );
+                  }
+                },
+              );
+            }
+          },
+        );
+      }
+    }
+
+    return Center(
+      child: Text('Giriş Başarılı!'),
     );
   }
 }
